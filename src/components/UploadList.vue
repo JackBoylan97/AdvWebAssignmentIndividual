d063950cf180a57a0a0100b035de67e536a8fdb6
Jack Boylan <35166529+JackBoylan97@users.noreply.github.com>
2021-02-21 23:42:34 +0000

    Updates
    -User can upload to database
    -Validation For user Upload Completed
    -Basic upload list,
    -

--- src/components/UploadList.vue
+++ src/components/UploadList.vue
@@ -1,62 +1,61 @@
 <template>
-    <div class="jumbotron text-left">
-      <table v-if="load" style="width:100%" >
-  <tr>
-    <th>Title</th>
-    <th>Details</th>
-    <th>Tags</th>
-    <th> Graph</th>
-  </tr>
-  <tr v-for="subs in userSubmissions" :key="subs" >
-    <td>{{subs.uploadTitle}}</td>
-    <td> {{subs.uploadDetails}}</td>
-    <td> {{subs.tags}}</td>
-    <td> <graph-creator v-bind:userSubmissions="subs"/></td>
-  </tr>
-</table>
+  <div class="jumbotron text-left">
+    <table v-if="load" style="width:100%">
+      <tr>
+        <th>Title</th>
+        <th>Details</th>
+        <th>Tags</th>
+        <th>Graph</th>
+      </tr>
+      <tr v-for="subs in userSubmissions" :key="subs">
+        <td>{{ subs.uploadTitle }}</td>
+        <td>{{ subs.uploadDetails }}</td>
+        <td>{{ subs.tags }}</td>
+        <td><graph-creator v-bind:userSubmissions="subs" /></td>
+        <td><button @click="deleteSelected(subs)"></button></td>
+      </tr>
+    </table>
   </div>
-<button  @click="doSomething">Do it</button>
 </template>
 <script>
 // @ is an alias to /src
-import GraphCreator from '../components/GraphCreator'
-import { getAllSubmissions } from "../firebase/database.js"
+import GraphCreator from "../components/GraphCreator";
+import { getAllSubmissions, deleteUserSubmission} from "../firebase/database.js";
 export default {
-  
   data() {
-      return{
-        userSubmissions:[],
-        load: false
-      }
+    return {
+      userSubmissions: [],
+      load: false,
+    };
   },
   components: {
-     GraphCreator
-    },
-    mounted(){
-      this.getInfo()
-    },
+    GraphCreator,
+  },
+  mounted() {
+    this.getInfo();
+  },
   methods: {
-      async getInfo(){
-      const userSubmissions = await getAllSubmissions()
+    async getInfo() {
+      const userSubmissions = await getAllSubmissions();
 
-        this.userSubmissions = userSubmissions
-        console.log(this.userSubmissions)
-        this.load = true
-        
-      }   
-  }
-}
+      this.userSubmissions = userSubmissions;
+      this.load = true;
+    },
+      deleteSelected(e) {
+        const selectedDoc = e.tags
+        console.log(selectedDoc)
+        deleteUserSubmission(selectedDoc)
+        return;
+      }
+  },
+};
 </script>
 
-
-
-
-
 <style scoped>
-  .jumbotron {
-    background-color: #5f75f1;
-    color: #fff;
-    padding: 100px 25px;
-    font-family: Montserrat, sans-serif;
-  }
+.jumbotron {
+  background-color: #5f75f1;
+  color: #fff;
+  padding: 100px 25px;
+  font-family: Montserrat, sans-serif;
+}
 </style>
--- src/firebase/database.js
+++ src/firebase/database.js
@@ -4,16 +4,16 @@ import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAGDp5Pf4L0JdY2BywfxDI88Abwy09i4G4",
-    authDomain: "adv-web-assignment.firebaseapp.com",
-    databaseURL: "https://adv-web-assignment-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "adv-web-assignment",
-    storageBucket: "adv-web-assignment.appspot.com",
-    messagingSenderId: "831433916933",
-    appId: "1:831433916933:web:c5d9e0caa636c03ebb126c",
-    measurementId: "G-J43RTZD1CW"
+  apiKey: "AIzaSyAGDp5Pf4L0JdY2BywfxDI88Abwy09i4G4",
+  authDomain: "adv-web-assignment.firebaseapp.com",
+  databaseURL:
+    "https://adv-web-assignment-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "adv-web-assignment",
+  storageBucket: "adv-web-assignment.appspot.com",
+  messagingSenderId: "831433916933",
+  appId: "1:831433916933:web:c5d9e0caa636c03ebb126c",
+  measurementId: "G-J43RTZD1CW",
 };
-
 
 // Initalize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -27,18 +27,29 @@ firebaseFireStore.settings({ timestampsInSnapshoot: true });
 export const createSubmission = (submission) => {
   //Add user ID property from auth
   submission.userId = firebaseAuthentication.currentUser.uid;
-  return userSubmissions.doc(submission.userId).collection("submissions").add(submission);
+  return userSubmissions
+    .doc(submission.userId)
+    .collection("submissions")
+    .add(submission);
 };
 
+export const deleteUserSubmission = async (selectedDoc) => {
+  userSubmissions.doc(firebaseAuthentication.currentUser.uid)
+    .collection("submissions")
+    .where("tags", "==", selectedDoc).get().then((qSnap)=>{
+       qSnap.docs.forEach(doc => doc.ref.delete())
+    });
+};
 
-
-export const getAllSubmissions = async () => { 
-  const query = await userSubmissions.doc(firebaseAuthentication.currentUser.uid).collection("submissions").where("userId", "==", firebaseAuthentication.currentUser.uid)
+export const getAllSubmissions = async () => {
+  const query = await userSubmissions
+    .doc(firebaseAuthentication.currentUser.uid)
+    .collection("submissions")
+    .where("userId", "==", firebaseAuthentication.currentUser.uid)
     .get();
   return query.docs.map((doc) => {
     return doc.data();
-  })
+  });
 };
 
-
-export { firebaseAuthentication, firebaseFireStore, timestamp};
+export { firebaseAuthentication, firebaseFireStore, timestamp };
