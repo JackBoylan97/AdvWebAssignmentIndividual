855cde54a8ad3e7b66bf740805dc5082b704027c
JackBoylanAdvWeb <79326540+JackBoylanAdvWeb@users.noreply.github.com>
2021-02-25 17:31:35 +0000

    Update database.js

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -5,16 +5,14 @@ import "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGDp5Pf4L0JdY2BywfxDI88Abwy09i4G4",
-  authDomain: "adv-web-assignment.firebaseapp.com",
-  databaseURL:
-    "https://adv-web-assignment-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "adv-web-assignment",
-  storageBucket: "adv-web-assignment.appspot.com",
-  messagingSenderId: "831433916933",
-  appId: "1:831433916933:web:c5d9e0caa636c03ebb126c",
-  measurementId: "G-J43RTZD1CW",
-};
+    apiKey: "AIzaSyCjnDHoaF4i8i6Dqkygoxh8RexNGO1L1Lw",
+    authDomain: "advwebindividual.firebaseapp.com",
+    projectId: "advwebindividual",
+    storageBucket: "advwebindividual.appspot.com",
+    messagingSenderId: "695153807335",
+    appId: "1:695153807335:web:88bd3bb70b8a2d84767742",
+    measurementId: "G-PK4WQPZMC9"
+  };
 
 
 // Initalize Firebase
